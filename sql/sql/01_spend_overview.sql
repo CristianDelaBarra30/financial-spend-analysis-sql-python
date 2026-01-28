7b06e7b78748d170be03caeaf6847f486ea6f938
Cristian De La Barra <cdelabd21@gmail.com>
2026-01-27 23:57:40 -0300

    Update 01_spend_overview.sql

--- sql/sql/01_spend_overview.sql
+++ sql/sql/01_spend_overview.sql
@@ -1,1 +1,10 @@
+-- 01_spend_overview.sql
+-- Objective: Analyze total and category-level spending
 
+SELECT
+    category,
+    SUM(actual_spend) AS total_actual_spend,
+    SUM(budget_spend) AS total_budget_spend
+FROM spend_data
+GROUP BY category
+ORDER BY total_actual_spend DESC;
